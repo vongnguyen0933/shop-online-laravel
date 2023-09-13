e867e88c19702145286fb905a7b335a4587dae08
vongnguyen0933 <vongnguyen0933@gmail.com>
2023-09-13 22:07:57 +0700

    2commit

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -10,8 +10,10 @@ use Illuminate\Support\Facades\Auth;
 class HomeController extends Controller
 {
     public function index(){
-        $admin = Auth::guard('admin')->user();
-        echo 'Chào mừng ' .$admin->name.' <a href="'.route('admin.logout').'">Logout</a>';
+        return view ('admin.dashboard');
+
+        // $admin = Auth::guard('admin')->user();
+        // echo 'Chào mừng ' .$admin->name.' <a href="'.route('admin.logout').'">Logout</a>';
     }
       public function logout(){
         Auth::guard('admin')->logout();
